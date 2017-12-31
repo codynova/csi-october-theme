fe56a863d735a4caf28306b6dace14d299c66357
Cody Persinger <codypersinger@gmail.com>
2017-12-31 13:24:14 -0600

    Updated intro/header media queries

--- themes/csi/assets/app.js
+++ themes/csi/assets/app.js
@@ -170,57 +170,57 @@ app.service('navigationService', [
                 }
             });
     
-            (function() {
-                var tabFadeObjects = [
-                    {
-                        class: 'fade-content-work',
-                        posClass: 'intro-basic',
-                        html: "<h2>When you're ready for <br><span class='purple'>a different breed of event.</span></h2>"
-                    },
-                    {
-                        class: 'fade-content-about',
-                        posClass: 'intro-about',
-                        html: "<h2>Concert, festival, and corporate events <br><span>in the Pacific Northwest.</span></h2>"
-                    },
-                    {
-                        class: 'fade-content-contact',
-                        posClass: 'intro-contact',
-                        html: "<h2>Drop us a line, <br><span>we'll be in touch.</span></h2>"
-                    }
-                ];
-                var navLinks = document.querySelectorAll('.fade-content');
-                var activeLink;
-                for (var i = 0; i < navLinks.length; i++) {
-                    if (navLinks[i].classList.contains('active')) activeLink = i;
-                }
-                var currentTabObject = tabFadeObjects[activeLink];
-                var introElement = document.querySelector('.intro');
-                setTimeout(function () {
-                    introElement.innerHTML = tabFadeObjects[activeLink].html;
-                    introElement.classList.add(currentTabObject.posClass);
-                    introElement.classList.remove('fade-out');
-                }, 300);
-                navLinks.forEach(function (navLink) {
-                    navLink.addEventListener('click', function (e) {
-                        if (e.target.classList.contains(currentTabObject.class)) return;
-                        introElement.classList.add('fade-out');
-                        function replaceIntroContent(i, oldTabObject) {
-                            introElement.classList.remove(oldTabObject.posClass);
-                            introElement.innerHTML = tabFadeObjects[i].html;
-                            introElement.classList.add(tabFadeObjects[i].posClass);
-                            introElement.classList.remove('fade-out');
-                        }
+            // (function() {
+            //     var tabFadeObjects = [
+            //         {
+            //             class: 'fade-content-work',
+            //             posClass: 'intro-basic',
+            //             html: "<h2>When you're ready for <br><span class='purple'>a different breed of event.</span></h2>"
+            //         },
+            //         {
+            //             class: 'fade-content-about',
+            //             posClass: 'intro-about',
+            //             html: "<h2>Concert, festival, and corporate events <br><span>in the Pacific Northwest.</span></h2>"
+            //         },
+            //         {
+            //             class: 'fade-content-contact',
+            //             posClass: 'intro-contact',
+            //             html: "<h2>Drop us a line, <br><span>we'll be in touch.</span></h2>"
+            //         }
+            //     ];
+            //     var navLinks = document.querySelectorAll('.fade-content');
+            //     var activeLink;
+            //     for (var i = 0; i < navLinks.length; i++) {
+            //         if (navLinks[i].classList.contains('active')) activeLink = i;
+            //     }
+            //     var currentTabObject = tabFadeObjects[activeLink];
+            //     var introElement = document.querySelector('.intro');
+            //     setTimeout(function () {
+            //         introElement.innerHTML = tabFadeObjects[activeLink].html;
+            //         introElement.classList.add(currentTabObject.posClass);
+            //         introElement.classList.remove('fade-out');
+            //     }, 300);
+            //     navLinks.forEach(function (navLink) {
+            //         navLink.addEventListener('click', function (e) {
+            //             if (e.target.classList.contains(currentTabObject.class)) return;
+            //             introElement.classList.add('fade-out');
+            //             function replaceIntroContent(i, oldTabObject) {
+            //                 introElement.classList.remove(oldTabObject.posClass);
+            //                 introElement.innerHTML = tabFadeObjects[i].html;
+            //                 introElement.classList.add(tabFadeObjects[i].posClass);
+            //                 introElement.classList.remove('fade-out');
+            //             }
     
-                        for (var i = 0; i < tabFadeObjects.length; i++) {
-                            if (e.target.classList.contains(tabFadeObjects[i].class)) {
-                                var oldTabObject = currentTabObject;
-                                currentTabObject = tabFadeObjects[i];
-                                setTimeout(replaceIntroContent.bind(null, i, oldTabObject), 300);
-                            }
-                        }
-                    });
-                });
-            })();
+            //             for (var i = 0; i < tabFadeObjects.length; i++) {
+            //                 if (e.target.classList.contains(tabFadeObjects[i].class)) {
+            //                     var oldTabObject = currentTabObject;
+            //                     currentTabObject = tabFadeObjects[i];
+            //                     setTimeout(replaceIntroContent.bind(null, i, oldTabObject), 300);
+            //                 }
+            //             }
+            //         });
+            //     });
+            // })();
         };
     }
 ]);
