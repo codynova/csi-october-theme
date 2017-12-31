7aa4f7b7bb20d8f947daa503d138cdc75518e5e6
Cody Persinger <codypersinger@gmail.com>
2017-12-31 16:39:53 -0600

    Updates to whatever the fuck

--- themes/csi/assets/app.js
+++ themes/csi/assets/app.js
@@ -58,6 +58,8 @@ app.service('dataService', [
                 $scope.workList = response.map(function(workItem) {
                     workItem.filters = workItem.filtertags.split(',');
                     return workItem;
+                }).sort(function(a, b) {
+                    return parseInt(b.order) - parseInt(a.order);
                 });
                 $scope.loadIncrement = WORK_LOAD_VALUES.increment;
                 $interval(function () {
@@ -94,53 +96,26 @@ app.service('dataService', [
 ]);
 
 app.service('scrollService', function() {
-   this.smoothScroll = function (destination) {
-       var duration = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 200;
-       var easing = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 'linear';
-       var callback = arguments[3];
-       var easings = {
-           linear: function linear(t) {
-               return t;
-           },
-           easeInCubic: function easeInCubic(t) {
-               return t * t * t;
-           },
-           easeOutCubic: function easeOutCubic(t) {
-               return --t * t * t + 1;
-           },
-           easeInOutCubic: function easeInOutCubic(t) {
-               return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
-           }
-       };
-       var start = window.pageYOffset;
-       var startTime = 'now' in window.performance ? performance.now() : new Date().getTime();
-       var documentHeight = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
-       var windowHeight = window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight;
-       var destinationOffset = typeof destination === 'number' ? destination : destination.offsetTop;
-       var destinationOffsetToScroll = Math.round(documentHeight - destinationOffset < windowHeight ? documentHeight - windowHeight : destinationOffset);
-       if ('requestAnimationFrame' in window === false) {
-           window.scroll(0, destinationOffsetToScroll);
-           if (callback) {
-               callback();
-           }
-           return;
-       }
-       function scroll() {
-           var now = 'now' in window.performance ? performance.now() : new Date().getTime();
-           var time = Math.min(1, (now - startTime) / duration);
-           var timeFunction = easings[easing](time);
-           window.scroll(0, Math.ceil(timeFunction * (destinationOffsetToScroll - start) + start));
-
-           if (window.pageYOffset === destinationOffsetToScroll) {
-               if (callback) {
-                   callback();
-               }
-               return;
-           }
-           requestAnimationFrame(scroll);
-       }
-       scroll();
-   }
+    this.init = function () {
+        var scrollService = this;
+        var workLinks = document.querySelectorAll('.scroll-to-work');
+        Array.prototype.map.call(workLinks, function (linkElement) {
+            linkElement.addEventListener('click', function (e) {
+                e.preventDefault();
+                scrollService.scrollToWork();
+            });
+        });    
+    },
+    this.scrollToWork = function () {
+        var topScrollPos = document.documentElement.scrollTop;
+        var viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+        var scrollValue = viewportHeight - topScrollPos;
+        window.scrollBy({ 
+          top: scrollValue,
+          left: 0, 
+          behavior: 'smooth' 
+        });        
+    }
 });
 
 app.service('mobileMenuService', function() {
@@ -160,67 +135,7 @@ app.service('navigationService', [
     'scrollService',
     function(scrollService) {
         this.init = function () {
-            var navLink = document.querySelector('.fade-content-work');
-            navLink.addEventListener('click', function(e) {
-                if (e.target.classList.contains('active')) {
-                    if (window.innerWidth > 960 && window.pageYOffset < 700) {
-                        // Smoothly scroll to 700px over 200ms
-                        scrollService.smoothScroll(700, 200, 'easeInCubic');
-                    }
-                }
-            });
-    
-            // (function() {
-            //     var tabFadeObjects = [
-            //         {
-            //             class: 'fade-content-work',
-            //             posClass: 'intro-basic',
-            //             html: "<h2>When you're ready for <br><span class='purple'>a different breed of event.</span></h2>"
-            //         },
-            //         {
-            //             class: 'fade-content-about',
-            //             posClass: 'intro-about',
-            //             html: "<h2>Concert, festival, and corporate events <br><span>in the Pacific Northwest.</span></h2>"
-            //         },
-            //         {
-            //             class: 'fade-content-contact',
-            //             posClass: 'intro-contact',
-            //             html: "<h2>Drop us a line, <br><span>we'll be in touch.</span></h2>"
-            //         }
-            //     ];
-            //     var navLinks = document.querySelectorAll('.fade-content');
-            //     var activeLink;
-            //     for (var i = 0; i < navLinks.length; i++) {
-            //         if (navLinks[i].classList.contains('active')) activeLink = i;
-            //     }
-            //     var currentTabObject = tabFadeObjects[activeLink];
-            //     var introElement = document.querySelector('.intro');
-            //     setTimeout(function () {
-            //         introElement.innerHTML = tabFadeObjects[activeLink].html;
-            //         introElement.classList.add(currentTabObject.posClass);
-            //         introElement.classList.remove('fade-out');
-            //     }, 300);
-            //     navLinks.forEach(function (navLink) {
-            //         navLink.addEventListener('click', function (e) {
-            //             if (e.target.classList.contains(currentTabObject.class)) return;
-            //             introElement.classList.add('fade-out');
-            //             function replaceIntroContent(i, oldTabObject) {
-            //                 introElement.classList.remove(oldTabObject.posClass);
-            //                 introElement.innerHTML = tabFadeObjects[i].html;
-            //                 introElement.classList.add(tabFadeObjects[i].posClass);
-            //                 introElement.classList.remove('fade-out');
-            //             }
-    
-            //             for (var i = 0; i < tabFadeObjects.length; i++) {
-            //                 if (e.target.classList.contains(tabFadeObjects[i].class)) {
-            //                     var oldTabObject = currentTabObject;
-            //                     currentTabObject = tabFadeObjects[i];
-            //                     setTimeout(replaceIntroContent.bind(null, i, oldTabObject), 300);
-            //                 }
-            //             }
-            //         });
-            //     });
-            // })();
+
         };
     }
 ]);
@@ -232,7 +147,7 @@ app.controller('primaryController', [
         // Navigation variables
         $scope.showNavCategories = true;
         $scope.filterQuery = '';
-        
+                
         // JSON data for work and navigation items
         dataService.fetchNav($scope);
         dataService.fetchWork($scope);
@@ -254,6 +169,7 @@ app.controller('primaryController', [
         $document.ready(function () {
             //mobileMenuService.animateHamburger();
             navigationService.init();
+            scrollService.init();
         });
     }
 ]);
